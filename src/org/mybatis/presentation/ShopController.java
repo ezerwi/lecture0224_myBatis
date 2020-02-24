4710fa04f2fb16829ba4ec133ac55a7e96d03384
johnwi <johnwi@knou.ac.kr>
2020-02-24 15:51:45 +0900

    ShopController.java : editForm(shopNo) / edit(shop) / remove(shopNo) 추가 , rearrange

--- src/org/mybatis/presentation/ShopController.java
+++ src/org/mybatis/presentation/ShopController.java
@@ -14,59 +14,81 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
-@RequestMapping(value = "/shop") // 요청하는 controller 중에 shop이 들어가면 이 class를 사용하도록 지정
+@RequestMapping(value = "/shop") // 요청하는 controller 중에 shop이 들어가면 이 class를 사용하도록
+									// 지정
 public class ShopController {
-	
+
 	// @Resource에 의해 ShopServiceImpl 객체를 자동 주입
 	@Resource
 	private ShopService shopService;
-	
+
 	// '/shop/list'를 get 방식으로 요청시 아래 method로 응답
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public ModelAndView list(Shop shop) throws Exception {
 		List<Shop> listShop = this.shopService.find(shop);
-		
+
 		ModelAndView mav = new ModelAndView("/shop/list");
 		mav.addObject("listShop", listShop);
-		
+
 		return mav;
 	}
-	
+
 	// 등록 화면 GET방식 접근 시 해당 view 만 전달 ; WEB-INF/jsp/shop/add.jsp
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
 	public String addForm() throws Exception {
 		return "/shop/add";
 	}
 
-	
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public ModelAndView add (Shop shop) throws Exception {
-		
+	public ModelAndView add(Shop shop) throws Exception {
+
 		// RedirectView의 경우 parameter까지 같이 전달됨,
-		// setExposeModelAttributes : RedirectView에 Parameter가 전달되도록 할지 설정 
+		// setExposeModelAttributes : RedirectView에 Parameter가 전달되도록 할지 설정
 		RedirectView rv = new RedirectView("list");
 		rv.setExposeModelAttributes(false);
-		
+
 		this.shopService.add(shop);
-		
+
 		return new ModelAndView(rv);
 	}
-	
-	
+
 	@RequestMapping(value = "/view/{shopNo}", method = RequestMethod.GET)
-	public ModelAndView view (@PathVariable String shopNo ) throws Exception {
+	public ModelAndView view(@PathVariable String shopNo) throws Exception {
 		ModelAndView mv = new ModelAndView("/shop/view");
-		
+
 		Shop shop = this.shopService.view(shopNo);
 		mv.addObject("shop", shop);
 
 		return mv;
 	}
-	
-	
-	@RequestMapping(value = "", method = RequestMethod.)
-	public () throws Exception {
-		
+
+	@RequestMapping(value = "/edit/{shopNo}", method = RequestMethod.GET)
+	public ModelAndView editForm(@PathVariable String shopNo) throws Exception {
+
+		ModelAndView mv = new ModelAndView("/shop/edit");
+
+		Shop shop = this.shopService.view(shopNo);
+		mv.addObject("shop", shop);
+		return mv;
 	}
-	
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST)
+	public ModelAndView edit(Shop shop) throws Exception {
+
+		RedirectView rv = new RedirectView("list");
+		rv.setExposeModelAttributes(false);
+
+		this.shopService.edit(shop);
+
+		return new ModelAndView(rv);
+	}
+
+	@RequestMapping(value = "/remove/{shopNo}", method = RequestMethod.GET)
+	public ModelAndView remove(@PathVariable String shopNo) throws Exception {
+		RedirectView rv = new RedirectView("../list");
+		rv.setExposeModelAttributes(false);
+		this.shopService.remove(shopNo);
+		return new ModelAndView(rv);
+	}
+
 }
